153ca8e8c03877bfaa6c0c8bd3385b11b73a8f15
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-08 12:31:51 +0200

    booking creation works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,11 +6,21 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @cat = Cat.find(params[:cat_id])
+    @booking.user = current_user
+    @booking.cat = @cat
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to cats_path
+      raise
     else
-      render "new"
+      render :new
     end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starting_date,:ending_date)
   end
 
 end
