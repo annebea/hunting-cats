514e89c8b5283d141b9862fb9d19b59b9e692336
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-04-10 14:33:33 +0200

    map ok but no markers

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,7 @@ class Cat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,55 +18,55 @@ User.destroy_all
 # )
 # toto.save!
 
-user0 = User.new(email: 'huey@gmail.com', password: 'azerty', first_name: 'Huey', last_name: 'Duck', phone: '0230405060', address: '7 Rue Louis Blanc, Nantes')
+user0 = User.new(email: 'huey@gmail.com', password: 'azerty', first_name: 'Huey', last_name: 'Duck', phone: '0230405060')
 user0.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/huey.png')), filename: 'huey.png', content_type: 'image/png')
 user0.save!
 
-user1 = User.new(email: 'dewey@gmail.com', password: 'azerty', first_name: 'Dewey', last_name: 'Duck', phone: '0230405060', address: '131 Boulevard Robert Schuman, Nantes')
+user1 = User.new(email: 'dewey@gmail.com', password: 'azerty', first_name: 'Dewey', last_name: 'Duck', phone: '0230405060')
 user1.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/dewey.png')), filename: 'dewey.png', content_type: 'image/png')
 user1.save!
 
-user2 = User.new(email: 'louie@gmail.com', password: 'azerty', first_name: 'Louie', last_name: 'Duck', phone: '0230405060', address: 'Donaldville')
+user2 = User.new(email: 'louie@gmail.com', password: 'azerty', first_name: 'Louie', last_name: 'Duck', phone: '0230405060')
 user2.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/louie.png')), filename: 'louie.png', content_type: 'image/png')
 user2.save!
 
-user3 = User.new(email: 'maud@gmail.com', password: 'azerty', first_name: 'Maud', last_name: 'De La Gournerie', phone: '0233454346', address: '2 place des Lices, Rennes')
+user3 = User.new(email: 'maud@gmail.com', password: 'azerty', first_name: 'Maud', last_name: 'De La Gournerie', phone: '0233454346')
 user3.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/maud.jpeg')), filename: 'maud.jpeg', content_type: 'image/jpeg')
 user3.save!
 
-user4 = User.new(email: 'vincent@gmail.com', password: 'azerty', first_name: 'Vincent', last_name: 'Guilleux', phone: '0233454346', address: '30 boulevard René Laennec, Rennes')
+user4 = User.new(email: 'vincent@gmail.com', password: 'azerty', first_name: 'Vincent', last_name: 'Guilleux', phone: '0233454346')
 user4.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/vincent.jpeg')), filename: 'vincent.jpeg', content_type: 'image/jpeg')
 user4.save!
 
-user5 = User.new(email: 'sebastien@gmail.com', password: 'azerty', first_name: 'Sebastien', last_name: 'Etienne', phone: '0233454346', address: '7 Avenue du General De Gaulle, Lannion')
+user5 = User.new(email: 'sebastien@gmail.com', password: 'azerty', first_name: 'Sebastien', last_name: 'Etienne', phone: '0233454346')
 user5.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/seb.jpg')), filename: 'seb.jpg', content_type: 'image/jpg')
 user5.save!
 
-user6 = User.new(email: 'anne@gmail.com', password: 'azerty', first_name: 'Anne', last_name: 'Mabire', phone: '0233454346', address: '4 rue du Centre, Saint-Gilles')
+user6 = User.new(email: 'anne@gmail.com', password: 'azerty', first_name: 'Anne', last_name: 'Mabire', phone: '0233454346')
 user6.avatar.attach(io: File.open(Rails.root.join('db/fixtures/users/anne.jpeg')), filename: 'anne.jpeg', content_type: 'image/jpeg')
 user6.save!
 
-cat1 = Cat.create(name: 'Dolby', description: 'Black and white cat', breed: 'Gouttiere', performance: 1, price_per_day: 20, status: 'available', age: 12, user_id: user2.id)
+cat1 = Cat.create(name: 'Dolby', description: 'Black and white cat', breed: 'Gouttiere', performance: 1, price_per_day: 20, status: 'available', age: 12, address: '131 Boulevard Robert Schuman, Nantes', user_id: user2.id)
 cat1.photo.attach(io: File.open(Rails.root.join('db/fixtures/cats/dolby.jpg')), filename: 'dolby.jpg', content_type: 'image/jpg')
 cat1.save!
 
-cat2 = Cat.create(name: 'Iago', description: 'Awesome hunter', breed: 'Bengal', performance: 4, price_per_day: 30, status: 'available', age: 6, user_id: user2.id)
+cat2 = Cat.create(name: 'Iago', description: 'Awesome hunter', breed: 'Bengal', performance: 4, price_per_day: 30, status: 'available', age: 6, address: '131 Boulevard Robert Schuman, Nantes', user_id: user2.id)
 cat2.photo.attach(io: File.open(Rails.root.join('db/fixtures/cats/iago.jpg')), filename: 'iago.jpg', content_type: 'image/jpg')
 cat2.save!
 
-cat3 = Cat.create(name: 'Moustache', description: 'Great at hunting', breed: 'Bengal', performance: 2, price_per_day: 26, status: 'available', age: 3, user_id: user4.id)
+cat3 = Cat.create(name: 'Moustache', description: 'Great at hunting', breed: 'Bengal', performance: 2, price_per_day: 26, status: 'available', age: 3, address: '30 boulevard René Laennec, Rennes', user_id: user4.id)
 cat3.photo.attach(io: File.open(Rails.root.join('db/fixtures/cats/moustache.jpg')), filename: 'moustache.jpg', content_type: 'image/jpg')
 cat3.save!
 
-cat4 = Cat.create(name: 'Poutine', description: 'THE BEST', breed: 'Maine coon', performance: 5, price_per_day: 45, status: 'available', age: 10, user_id: user4.id)
+cat4 = Cat.create(name: 'Poutine', description: 'THE BEST', breed: 'Maine coon', performance: 5, price_per_day: 45, status: 'available', age: 10, address: '30 boulevard René Laennec, Rennes', user_id: user4.id)
 cat4.photo.attach(io: File.open(Rails.root.join('db/fixtures/cats/poutine.jpg')), filename: 'poutine.jpg', content_type: 'image/jpg')
 cat4.save!
 
-cat5 = Cat.create(name: 'Staline', description: 'Mice, be scared!', breed: 'Bengal', performance: 4, price_per_day: 35, status: 'available', age: 8, user_id: user4.id)
+cat5 = Cat.create(name: 'Staline', description: 'Mice, be scared!', breed: 'Bengal', performance: 4, price_per_day: 35, status: 'available', age: 8, address: '30 boulevard René Laennec, Rennes', user_id: user4.id)
 cat5.photo.attach(io: File.open(Rails.root.join('db/fixtures/cats/staline.jpg')), filename: 'staline.jpg', content_type: 'image/jpg')
 cat5.save!
 
-cat6 = Cat.create(name: 'Mrs Fluffton', description: 'Hunt with style', breed: 'Brittish', performance: 3, price_per_day: 32, status: 'available', age: 5, user_id: user6.id)
+cat6 = Cat.create(name: 'Mrs Fluffton', description: 'Hunt with style', breed: 'Brittish', performance: 3, price_per_day: 32, status: 'available', age: 5, address: '4 rue du Centre, Saint-Gilles', user_id: user6.id)
 cat6.photo.attach(io: File.open(Rails.root.join('db/fixtures/cats/mrs-fluffton.jpg')), filename: 'mrs-fluffton.jpg', content_type: 'image/jpg')
 cat6.save!
 
